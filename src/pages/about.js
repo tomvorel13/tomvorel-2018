0265199408ebb73460dba138638b6e2fc64201e4
Tom Vorel <tomvorel13@gmail.com>
2018-08-09 18:14:45 +0200

    Fixing a z-index bug

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { colors } from '../utilities'
-import { MainWrapper, ContentColumn, SectionHeading, Text } from '../styles/pages'
+import {
+  MainWrapper,
+  ContentColumn,
+  SectionHeading,
+  Text,
+} from '../styles/pages'
 
 const About = () => (
   <MainWrapper>
@@ -10,22 +15,24 @@ const About = () => (
       <SectionHeading>About</SectionHeading>
       <Text>
         Hi, I’m Tom Vorel. <br />
-        <br /> I’m a passionate self-taught Frontend Web Developer currently
-        working full time for Monster Worldwide, where I’m mostly responsible
-        for creating custom, awesome looking and responsive job ads for our
-        premium customers such are Huawei, Spotify, FedEx and many others.{' '}
+        <br /> I’m a passionate, self-taught Frontend Web Developer that
+        currently works full time for Monster Worldwide. I’m a member of the
+        team responsible for creating custom job ads that are awesome looking
+        and responsive for our premium customers, such as Huawei, Spotify,
+        FedEx, and many others.
         <br />
-        <br /> In my job I mostly work with HTML and CSS (including the newest
-        features such as flexbox, CSS Animations and hopefully we can start
-        implementing CSS Grid in the near future). When it comes to my free time I’m
-        spending most of it working on my Javascript skills and developing
-        single page applications using mostly React, although I also have
-        experience with Angular 2+ and Vue.js. <br />
-        <br /> When I’m not coding I love reading about web development on
-        Twitter and Medium and listening to about 20 different podcasts. When I
-        want to take a break from programming altogether, I love watching sports
-        (Arsenal FC and Detroit Pistons are my 2 favorite professional teams) or
-        working out in the gym. <br />
+        <br /> In my day-to-day work at Monster, I mostly write code with HTML
+        and CSS (including the newest features such as flexbox, CSS Animations
+        and hopefully we will start implementing CSS Grid in the near future).{' '}
+        <br />
+        <br /> When it comes to my free time; I’m spending most of it working on
+        my Javascript skills, developing single page applications using React,
+        and I also have experience with Angular 2+ and Vue.js. <br /> <br />
+        When I’m not coding; I love reading about web development on Twitter and
+        Medium, listening to about 20 different podcasts and when I want to take
+        a break from programming altogether, I love watching sports (Arsenal FC
+        and Detroit Pistons are my 2 favorite professional teams) or working out
+        in the gym. <br />
         <br /> I’m also very interested in self-development and I love coffee!
       </Text>
     </ContentColumn>
@@ -35,5 +42,3 @@ const About = () => (
 export default About
 
 // STYLES
-
-
